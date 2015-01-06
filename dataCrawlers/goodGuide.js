20cfa1bae7d482c8e4a8f3c5aa4dba9881c42a38
BenAllenUK <external@benallen.info>
2015-01-06 22:39:32 +0000

    Added roundtable of palm oil & moved web scrapers
    Will check for a company name to see if they are a member of the palm
    oil sustainability group.
    
    Moved web scrapers into a folder since we will probably get a lot more

--- dataCrawlers/goodGuide.js
+++ dataCrawlers/goodGuide.js
@@ -6,6 +6,7 @@ will return the good guides score on a product
 first number represents a health score out of 10
 second number represents a enviromental score out of 10
 third number represents a society score out of 10
+
 */
 var http = require("http"),
 	request = require('request'),
@@ -24,18 +25,21 @@ request(url, function(error, response, body) {
 	if (!error && response.statusCode == 200) {
 
 		var $ = cheerio.load(body);
-		var productUrl = "http://www.goodguide.com/" + $('.search-tile-content').find('.search-tile .image-container a').attr('href');
+		if($('.search-tile-content .search-tile').length > 0) {
+			var productUrl = "http://www.goodguide.com/" + $('.search-tile-content').find('.search-tile .image-container a').attr('href');
 
-		request(productUrl, function(error, response, body) {
-			if (!error && response.statusCode == 200) {
-				var $ = cheerio.load(body);
-				var ratings = [];
-				$('.rating-cell').each(function(){
-					ratings.push($(this).text().replace('\n','').replace('\n',''));
-				});
-				console.log(ratings);
-			}
-		});
-		
+			request(productUrl, function(error, response, body) {
+				if (!error && response.statusCode == 200) {
+					var $ = cheerio.load(body);
+					var ratings = [];
+					$('.rating-cell').each(function(){
+						ratings.push($(this).text().replace('\n','').replace('\n',''));
+					});
+					console.log(ratings);
+				}
+			});
+		} else {
+			console.log("No products found");
+		}
 	}
 });
---
+++ dataCrawlers/roundtablePalmOil.js
@@ -0,0 +1,32 @@
+/*
+node roundtablePalmOil.js [company name] 
+
+returns Y or N depending on whether the company is a member of the round table
+
+*/
+var http = require("http"),
+	request = require('request'),
+	cheerio = require("cheerio"),
+	urlParser = require("url");
+	
+// Grab all the inputs from the console
+// only productname is required
+var query = encodeURIComponent(process.argv[2]);
+
+// Create url
+var url = "http://www.rspo.org/members?keywords="+query;
+
+// Call web request
+request(url, function(error, response, body) {
+	if (!error && response.statusCode == 200) {
+
+		var $ = cheerio.load(body);
+	
+		if ($('.table-responsive table tbody tr').length > 0){
+			console.log("Y");
+		} else {
+			console.log("N");
+		}
+		
+	}
+});
